62c41dc2c65b9dcaf4922aa166ee96b7069993e5
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-10-30 11:45:13 -1000

    fixed R graph

--- pca.R
+++ pca.R
@@ -41,7 +41,6 @@ y_pred = predict(classifier, newdata = test_set[-3])
 cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
-#install.packages('ElemStatLearn')
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -50,7 +49,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('PC1', 'PC2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Kernel SVM (Training set)',
+     main = 'SVM (Training set)',
      xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -65,8 +64,7 @@ X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('PC1', 'PC2')
 y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'Kernel SVM (Test set)',
+plot(set[, -3], main = 'SVM (Test set)',
      xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
