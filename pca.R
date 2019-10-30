6c66860e3900c27edacf0009513698e1f8d82900
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-10-30 10:07:49 -1000

    R template finished

--- pca.R
+++ pca.R
@@ -7,40 +7,41 @@ dataset = read.csv('Wine.csv')
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.80)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-training_set[-14] = scale(training_set[-14])
-test_set[-14] = scale(test_set[-14])
+#Feature Scaling
+training_set[,-14] = scale(training_set[,-14])
+test_set[,-14] = scale(test_set[,-14])
 
 # Applying PCA
-# install.packages('caret')
+#install.packages('caret')
 library(caret)
-# install.packages('e1071')
+#install.packages('e1071')
 library(e1071)
 pca = preProcess(x = training_set[-14], method = 'pca', pcaComp = 2)
 training_set = predict(pca, training_set)
-training_set = training_set[c(2, 3, 1)]
+training_set = training_set[c(2,3,1)]
 test_set = predict(pca, test_set)
-test_set = test_set[c(2, 3, 1)]
+test_set = test_set[c(2,3,1)]
 
-# Fitting SVM to the Training set
-# install.packages('e1071')
+#Fitting Classifier to the Training set
+#install.packages('e1071')
 library(e1071)
 classifier = svm(formula = Customer_Segment ~ .,
                  data = training_set,
                  type = 'C-classification',
-                 kernel = 'linear')
+                 kernal = 'radial')
 
-# Predicting the Test set results
+#Predicting the test set results
 y_pred = predict(classifier, newdata = test_set[-3])
 
-# Making the Confusion Matrix
+#Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
+#install.packages('ElemStatLearn')
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -49,7 +50,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('PC1', 'PC2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'SVM (Training set)',
+     main = 'Kernel SVM (Training set)',
      xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -64,7 +65,8 @@ X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('PC1', 'PC2')
 y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'SVM (Test set)',
+plot(set[, -3],
+     main = 'Kernel SVM (Test set)',
      xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
